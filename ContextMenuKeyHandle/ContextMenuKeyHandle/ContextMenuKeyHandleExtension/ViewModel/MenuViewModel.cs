ed57a0b522520c9eb1ce5bb04c10d058fada1ce6
OKUI Note PC <okui@jpnsys.co.jp>
2022-09-07 14:58:13 +0900

    KeyHandleExtension追加

---
+++ ContextMenuKeyHandle/ContextMenuKeyHandle/ContextMenuKeyHandleExtension/ViewModel/MenuViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ContextMenuKeyHandle.ViewModel
+{
+  public class MenuViewModel
+  {
+    public string MenuText
+    {
+      get;
+      set;
+    }
+
+    public Key Key
+    {
+      get;
+      set;
+    }
+
+    public string KeyGesture
+    {
+      get
+      {
+        return Key.ToString();
+      }
+    }
+
+    public ICommand KeyCommand
+    {
+      get;
+      set;
+    }
+
+    public ICommand ClickCommand
+    {
+      get;
+      set;
+    }
+  }
+}
